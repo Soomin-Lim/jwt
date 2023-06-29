c8a7ca154c50b7cb6e6c66ec2aeb9bcb57c5c2d4
Soomin-Lim <augustth511@gmail.com>
2023-06-29 19:16:36 +0900

    섹션 3. 스프링 시큐리티 JWT 서버 구축 - 20강 JWT를 위한 security 설정

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.cos.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 x
+                .and()
+                .addFilter(corsFilter) // 모든 요청은 이 필터를 거친다
+                .formLogin().disable() // 폼(태크) 로그인 x
+                .httpBasic().disable() // 기본적인 http 로그인 방식 x
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and().build();
+    }
+}
